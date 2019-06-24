53eb289b22c804b0ab6a9f59c81320745f7b9192
Robert <r.haynes@f5.com>
2019-06-24 16:21:13 -0700

    Update setpw.sh
    added resource provisioning and save sys config

--- declarations/setpw.sh
+++ declarations/setpw.sh
@@ -1,2 +1,4 @@
 ssh admin@bigip.example.com modify auth user admin password cowpigdog
 export BIGPASS=cowpigdog
+ssh admin@bigip.example.com modify sys provision asm level nominal
+ssh admin@bigip.example.com  save sys config
