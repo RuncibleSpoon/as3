b95849fce347aff4339751f0f35d89cf34860547
Robert <r.haynes@f5.com>
2019-06-25 10:32:00 -0700

    changed setpw to setup
    added enviornment variable, etc.

--- setpw.sh
+++
@@ -1,4 +0,0 @@
-ssh admin@bigip.example.com modify auth user admin password cowpigdog
-export BIGPASS=cowpigdog
-ssh admin@bigip.example.com modify sys provision asm level nominal
-ssh admin@bigip.example.com  save sys config
