5af79b40a9659d3d077a21ad1e57dc39ade8b07d
Robert <r.haynes@f5.com>
2019-06-26 09:26:38 -0700

    Update locustfile.py

--- tests/locustfile.py
+++ tests/locustfile.py
@@ -1,19 +1,6 @@
 from locust import HttpLocust, TaskSet, task
 
 class UserBehavior(TaskSet):
-    def on_start(self):
-        """ on_start is called when a Locust start before any task is scheduled """
-       # self.login()
-
-    def on_stop(self):
-        """ on_stop is called when the TaskSet is stopping """
-        
-
-    def login(self):
-        self.client.post("/secure", {"username":"user", "password":"user"})
-
-    def logout(self):
-        self.client.post("/logout", {"username":"ellen_key", "password":"education"})
 
     @task(2)
     def index(self):
